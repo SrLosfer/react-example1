ef3471ccea7c6755083f53d3b48b08269d659c2c
Losfer <70660886+Losfer@users.noreply.github.com>
2021-06-17 23:42:39 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>esta es mi aplicacion</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
