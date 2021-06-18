9a9687b632f4ecff5c5df338992f02b1f86316c1
Losfer <70660886+Losfer@users.noreply.github.com>
2021-06-17 23:53:36 -0500

    cambio de contenido

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>esta es mi aplicacion</h1>
+        <h1>esta es mi aplicacion subida a github para ser hosteada</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
