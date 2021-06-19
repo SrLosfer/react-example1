b710eaff50401d640f0de44edb661cdd2cf23e3e
Losfer <70660886+Losfer@users.noreply.github.com>
2021-06-18 23:07:37 -0500

    Fixed Home

--- src/App.js
+++ src/App.js
@@ -21,18 +21,18 @@ function App() {
           <div className="Titulo">
             <img src={logo} className="App-logo" alt="logo" />
             <div className="btn-group">
-            <NavLink to="/" className="btn btn-dark m-5" activeClassName="active">
+            <NavLink to="/react-example/" className="btn btn-dark m-5" activeClassName="active">
                   <h5 className="bg-dark m-1">Home</h5>
                 </NavLink>
-                <NavLink to="react-example1/src/components/AboutMe" className="btn btn-dark m-5" activeClassName="active">
+                <NavLink to="react-example1/AboutMe" className="btn btn-dark m-5" activeClassName="active">
                   <h5 className="bg-dark m-1">About Me</h5>
 
                 </NavLink>
-                <NavLink to="react-example1/src/components/MyHobbies" className="btn btn-dark m-5" activeClassName="active">
+                <NavLink to="react-example1/MyHobbies" className="btn btn-dark m-5" activeClassName="active">
                   <h5 className="bg-dark m-1">My Hobbie</h5>
 
                 </NavLink>
-                <NavLink to="react-example1/src/components/ContactMe" className="btn btn-dark m-5" activeClassName="active">
+                <NavLink to="react-example1/ContactMe" className="btn btn-dark m-5" activeClassName="active">
                   <h5 className="bg-dark m-1">Contact Me</h5>
 
                 </NavLink>
@@ -42,16 +42,16 @@ function App() {
         </header>
         <body>
           <Switch>
-            <Route path="/" exact>
+            <Route path="/react-example1/" exact>
               <Home/>
             </Route>
-            <Route path="react-example1/src/components/AboutMe" exact>
+            <Route path="react-example1/AboutMe" exact>
               <AboutMe />
             </Route>
-            <Route path="react-example1/src/components/MyHobbies" exact>
+            <Route path="react-example1/MyHobbies" exact>
               <MyHobbies />
             </Route>
-            <Route path="react-example1/src/components/ContactMe" exact>
+            <Route path="react-example1/ContactMe" exact>
               <ContactMe />
             </Route>
           </Switch>
