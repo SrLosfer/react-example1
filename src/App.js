0dcb6eee4ba555fd01227dabecd14fa72b373072
Losfer <70660886+Losfer@users.noreply.github.com>
2021-06-18 21:04:24 -0500

    app.js fixed

--- src/App.js
+++ src/App.js
@@ -39,19 +39,6 @@ function App() {
             </div>
 
           </div>
-          
-          <h2>Contenido del curriculum aqui</h2>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
         <body>
           <Switch>
